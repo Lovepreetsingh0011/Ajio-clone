cb5acaeaf96e323743438d1819efa1069e0ef225
Lovepreet Singh <lovepreetsin9264@gmail.com>
2024-09-18 11:28:59 +0530

    index2frile

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import { app } from "./Src/App.js";
 import "dotenv/config";
 
 import { dbconnetion } from "./Src/Db/db.js";
-
+const app = "lovy";
 dbconnetion().then(() => {
   app.listen(process.env.PORT, () => {
     console.log("Appliction Listen At Port no", process.env.PORT);
